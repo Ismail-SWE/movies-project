056322eecaa230889284d56c2c1e68e7572bb199
Ismailov <ismail.swe.uz@gmail.com>
2026-05-13 19:50:57 +0500

    new changes

--- movies/src/components/app/app.jsx
+++ movies/src/components/app/app.jsx
@@ -1,17 +1,20 @@
-import AppFilter from "../app-filter/app-filter";
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+
 
 
 
 const App = ()=>{
 return (
-  <div>
-
-    <h1>Hello Babe!!!</h1>
-    <AppFilter/>
-    <AppInfo/>
-    <SearchPanel/>
+  <div className="app">
+    <div>
+      <AppInfo/>
+     <div>
+      <SearchPanel/>
+      <AppFilter/>
+     </div> 
+    </div>
   </div>
 
 
